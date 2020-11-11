92d76f96d999ca0ce13b981501636bbbff93cf30
Vitor Machado <vitormds22@gmail.com>
2020-11-10 23:43:17 -0300

    Converti o input do usuario para inteiro, finalizando a validaçao

--- python3/jogos/adivanhacao.py
+++ python3/jogos/adivanhacao.py
@@ -5,11 +5,12 @@ print('*********************************')
 numero_secreto = 42
 print('O tipo da variável é: ', type(numero_secreto))
 
-chute = input('Digite o seu numero:')
-print('O tipo do input é: ', type(chute))
+chute_str = input('Digite o seu numero:')
+print('O tipo do input é: ', type(chute_str))
 
-print('Você digitou: ',chute)
+print('Você digitou: ',chute_str)
 
+chute = int(chute_str)
 if (numero_secreto == chute):
     print('Você acertou')
 else:
