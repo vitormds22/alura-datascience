28f67cf990446fc0f3c0ea0a0e5820150cfacf67
Vitor Machado <vitormds22@gmail.com>
2020-11-10 23:39:19 -0300

    Inserindo a funçao type para analisar os tipos

--- python3/jogos/adivanhacao.py
+++ python3/jogos/adivanhacao.py
@@ -3,8 +3,10 @@ print('Bem vindo ao jogo de adivinhação!')
 print('*********************************')
 
 numero_secreto = 42
+print('O tipo da variável é: ', type(numero_secreto))
 
 chute = input('Digite o seu numero:')
+print('O tipo do input é: ', type(chute))
 
 print('Você digitou: ',chute)
 
