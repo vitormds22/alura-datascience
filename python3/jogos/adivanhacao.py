61b29c4dfa675b66b5c076acf79dcb702aedd6de
Vitor Machado <vitormds22@gmail.com>
2020-11-10 23:30:40 -0300

    Pedindo um chute para o usuario

--- python3/jogos/adivanhacao.py
+++ python3/jogos/adivanhacao.py
@@ -1,1 +1,9 @@
+print('*********************************')
 print('Bem vindo ao jogo de adivinhação!')
+print('*********************************')
+
+numero_secreto = 42
+
+chute = input('Digite o seu numero:')
+
+print('Você digitou: ',chute)
