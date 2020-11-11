3ad1a3ed4f34006b0d812eb1265ffeee4b4dcbf5
Vitor Machado <vitormds22@gmail.com>
2020-11-10 23:36:13 -0300

    Comparando a variavel e o input, com erro proposital de tipagem

--- python3/jogos/adivanhacao.py
+++ python3/jogos/adivanhacao.py
@@ -7,3 +7,8 @@ numero_secreto = 42
 chute = input('Digite o seu numero:')
 
 print('Você digitou: ',chute)
+
+if (numero_secreto == chute):
+    print('Você acertou')
+else:
+    print('Você errou')
