a434a54b68acd190b1dcbaa5778fab01b06f926e
Vitor Machado <vitormds22@gmail.com>
2020-11-12 23:55:16 -0300

    Melhorando a legibilidade das condicionais com variaveis

--- python3/jogos/adivanhacao.py
+++ python3/jogos/adivanhacao.py
@@ -11,12 +11,17 @@ print('O tipo do input é: ', type(chute_str))
 print('Você digitou: ',chute_str)
 
 chute = int(chute_str)
-if (numero_secreto == chute):
+
+acertou = numero_secreto == chute
+chute_maior = chute > numero_secreto
+chute_menor = chute < numero_secreto
+
+if (acertou):
     print('Você acertou')
 else:
-    if(chute > numero_secreto):
+    if(chute_maior):
         print('Você errou! O seu chute foi maior que o número secreto!')
-    if(chute < numero_secreto):
+    if(chute_menor):
         print('Você errou! O seu chute foi menor que o número secreto!')
 
 print('Fim do jogo!')
