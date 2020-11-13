5ce112270d28b2ce23a3e58d2bb371d328184c74
Vitor Machado <vitormds22@gmail.com>
2020-11-12 23:51:44 -0300

    Dando uma dica para o jogador

--- python3/jogos/adivanhacao.py
+++ python3/jogos/adivanhacao.py
@@ -14,6 +14,9 @@ chute = int(chute_str)
 if (numero_secreto == chute):
     print('Você acertou')
 else:
-    print('Você errou')
+    if(chute > numero_secreto):
+        print('Você errou! O seu chute foi maior que o número secreto!')
+    if(chute < numero_secreto):
+        print('Você errou! O seu chute foi menor que o número secreto!')
 
 print('Fim do jogo!')
