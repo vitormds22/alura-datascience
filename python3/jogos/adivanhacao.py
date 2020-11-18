7a145aa2d3e0314246d5044861e9d59a916d2634
Vitor Machado <vitormds22@gmail.com>
2020-11-17 23:59:13 -0300

    Inserindo o comando break dentro da condiçao de acerto da tentativa

--- python3/jogos/adivanhacao.py
+++ python3/jogos/adivanhacao.py
@@ -19,6 +19,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (acertou):
         print('Você acertou')
+        break
     else:
         if(chute_maior):
             print('Você errou! O seu chute foi maior que o número secreto!')
