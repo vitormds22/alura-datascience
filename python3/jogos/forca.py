02cf46c9346de806733466db65557b3e09cdff70
Vitor Machado <vitormds22@gmail.com>
2020-11-21 23:48:18 -0300

    Treinando a logica de looping para começar o jogo da forca

--- python3/jogos/forca.py
+++ python3/jogos/forca.py
@@ -6,10 +6,19 @@ def jogar():
     palavra_secreta = "banana"
     enforcou = False
     acertou = False
+    
+    # for letra in palavra_secreta:
+    #     print(letra)
 
     #enquanto(true)
     while(not enforcou and not acertou):
-        print('jogando...')
+        chute = input('Chute uma letra: ')
+        index = 0
+
+        for letra in palavra_secreta:
+            if(letra == chute):
+                print('Encontrei a letra {} na posicao {}'.format(letra, index))
+            index = index + 1
 
 if(__name__ == "__main__"):
     jogar()
