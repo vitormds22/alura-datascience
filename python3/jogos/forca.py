57a0ad3d8f8018d38e9edf91fee66cc72dda7c63
Vitor Machado <vitormds22@gmail.com>
2020-11-21 23:22:31 -0300

    Inserindo o loop para executar o jogo

--- python3/jogos/forca.py
+++ python3/jogos/forca.py
@@ -3,5 +3,13 @@ def jogar():
     print('***Bem vindo ao jogo de Forca!***')
     print('*********************************')
 
+    palavra_secreta = 'banana'
+    enforcou = False
+    acertou = False
+
+    #enquanto(true)
+    while(not enforcou and not acertou):
+        print('jogando...')
+
 if(__name__ == "__main__"):
     jogar()
