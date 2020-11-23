dd9f6af7f8f4796d545c004231c4bd6b80e6b924
maspTI <51337728+maspTI@users.noreply.github.com>
2020-11-23 18:49:31 -0300

    Adicionando funcionalidades de tentativas para se enforcar e tratando o loop infinito

--- python3/jogos/forca.py
+++ python3/jogos/forca.py
@@ -8,19 +8,28 @@ def jogar():
         
     enforcou = False
     acertou = False
+    erros = 0
     
-    while(not enforcou and not acertou):
+    while (not enforcou and not acertou):
         chute = input('Chute uma letra: ')
         chute = chute.lower().strip()
         
-        index = 0
-
-        for letra in palavra_secreta:
-            if(letra.lower() == chute):
-                letras_acertadas[index] = letra
-            index = index + 1
-
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(letra.lower() == chute):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            ##Se o chute não estiver dentro da palavra secreta, então sai adiciona mais um erro
+            erros += 1
+        ##Quando os erros atingirem o numero 6 a var enforcou se torna True consequentemente saindo do laço
+        enforcou = erros == 6
         print(letras_acertadas)
+    
+
+    print('Fim do jogo!')
+        
 
 if(__name__ == "__main__"):
     jogar()
