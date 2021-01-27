e65e07534e1f7ebeab55cb3fb8acfd0f69d35004
Vitor Machado <vitormds22@gmail.com>
2021-01-26 22:34:32 -0300

    Trabalhando com encapsulamento, atributos privados

--- python3/oo/conta.py
+++ python3/oo/conta.py
@@ -2,16 +2,16 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print('Construindo um objeto ... {}'.format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print('Saldo {} do titular {}'.format(self.saldo, self.titular))
+        print('Saldo {} do titular {}'.format(self.__saldo, self.__titular))
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
     
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
