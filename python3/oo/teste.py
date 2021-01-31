e13fcdd3799f532e7e9e7f04377963cdd5741518
Vitor Machado <vitormds22@gmail.com>
2021-01-31 14:49:46 -0300

    Aplicando o conceito de Herança

--- python3/oo/teste.py
+++ python3/oo/teste.py
@@ -1,27 +1,12 @@
-class Conta:
-    
-    def __init__(self, numero, saldo, limite):
-        self.__numero = numero 
-        self.__saldo = saldo 
-        self.__limite = limite
-        self.__tarifa_tranferencia = 8.0
+class Pessoa:
+    tamanho_cpf = 11
 
-    def __tem_saldo_disponivel_para(self, valor):
-        return valor <= (self.__limite + self.__saldo)
-    
-    def saca(self, valor):
-        if self.__tem_saldo_disponivel_para(valor):
-            self.__saldo -= valor
-            print('Saque efetuado')
-        else:
-            print('Saldo insuficiente')
+p = Pessoa()
 
-    def transferir(self, valor, destino):
-        valor_total = valor + self.__tarifa_tranferencia
+print(p.tamanho_cpf)
 
-        if self.__tem_saldo_disponivel_para(valor):
-            self.__saldo -= valor_total
-            print('Transferência efetuada.')
-        else:
-            print('Saldo insuficiente')
-    
+p.tamanho_cpf = 12
+
+print(p.tamanho_cpf)
+
+print(Pessoa.tamanho_cpf)
--- python3/oo2/modelo.py
+++ python3/oo2/modelo.py
@@ -1,29 +1,33 @@
-class Filme: 
-    
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano 
-        self.duracao = duracao
-        self.__likes = 0
+class Programa:
 
+    def __initi__(self, nome, ano):
+        self.__nome = nome
+        self.ano = ano
+        self.__likes = 0
+    
     def add_likes(self):
         self.__likes += 1
-
-    @property
-    def likes(self):
-        return self.__likes
 
     @property
     def nome(self):
         return self.__nome
 
+    @property
+    def likes(self):
+        return self.__likes
+
     @nome.setter
     def nome(self, nome):
         self.__nome = nome.title()
 
-    
+class Filme(Programa): 
+     def __init__(self, nome, ano, duracao):
+        self.__nome = nome.title()
+        self.ano = ano 
+        self.duracao = duracao
+        self.__likes = 0
 
-class Serie:
+class Serie(Programa):
 
     def __init__(self, nome, ano, temporadas):
         self.__nome = nome.title()
@@ -31,25 +35,11 @@ class Serie:
         self.temporadas = temporadas
         self.__likes = 0
 
-    def add_likes(self):
-        self.__likes += 1
-    
-    @property
-    def likes(self):
-        return self.__likes
-
-    @property
-    def nome(self):
-        return self.__nome
-    
-    @nome.setter
-    def nome(self,nome):
-        self.__nome = nome.title()
-
 vingadores = Filme('o menino de pijama listrado', 2006, 160)
 vingadores.add_likes()
 print(f'*FILME* \nFilme: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}\n')
 
 breaking_bad = Serie('breaking bad', 2008, 5)
+breaking_bad.add_likes()
 breaking_bad.add_likes()
 print(f'*SERIE* \nSérie: {breaking_bad.nome} - Ano: {breaking_bad.ano} - Temporadas: {breaking_bad.temporadas} - Likes: {breaking_bad.likes}')
